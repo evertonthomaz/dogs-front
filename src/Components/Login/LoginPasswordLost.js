cc7d8ba6418ae31e313269782c0fb268ab38a90e
Everton Thomáz <everton.thomaz@outlook.com>
2022-10-07 14:14:21 -0300

    criando tela para recuperar senha

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
-const LoginPasswordLoss = () => {
-  return <div></div>;
+const LoginPasswordLost = () => {
+  const email = useForm("email");
+
+  function handleSubmit(){
+    
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form>
+        <Input label="Email / Usuário" type="text" name="email" {...email} />
+        <Button>Enviar Email</Button>
+      </form>
+    </section>
+  );
 };
 
-export default LoginPasswordLoss;
+export default LoginPasswordLost;
