96e94ffd8a139c9e7ab4bc554180927ed4ed41af
Everton Thomáz <everton.thomaz@outlook.com>
2022-10-24 18:55:17 -0300

    add recuperar senha e head para atualizar o title no navegador

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -2,21 +2,45 @@ import React from "react";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
-
+import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_LOST } from "../../Api";
+import Error from "../Helper/Error";
+import Head from "../Helper/Head";
 const LoginPasswordLost = () => {
-  const email = useForm("email");
+  const email = useForm();
+  const { data, loading, error, request } = useFetch();
 
-  function handleSubmit(){
-    
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (email.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: email.value,
+        url: window.location.href.replace("perdeu", "resetar"),
+      });
+
+      await request(url, options);
+    }
   }
 
   return (
     <section>
+      <Head title="Perdeu a Senha?" />
       <h1 className="title">Perdeu a senha?</h1>
-      <form>
-        <Input label="Email / Usuário" type="text" name="email" {...email} />
-        <Button>Enviar Email</Button>
-      </form>
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="email" {...email} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+
+      <Error error={error} />
     </section>
   );
 };
